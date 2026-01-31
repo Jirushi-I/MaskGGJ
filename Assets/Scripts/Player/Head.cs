4a5785668b020483a299e19af7fe5bc67ac8fe35
Jirushi <61369895+Jirushi-I@users.noreply.github.com>
2026-01-31 16:41:33 -0500

    Movement And Camera

---
+++ Assets/Scripts/Player/Head.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEditor.Build;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Head : MonoBehaviour
+{
+    CharacterController controller;
+    
+    float yaw;
+    float pitch;
+
+    public float sensitivity = 1f;
+    public float minPitch = 45f;
+    public float maxPitch = 45f;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void Rotate(InputAction.CallbackContext context)
+    {
+        Vector2 input = context.ReadValue<Vector2>() * sensitivity * Time.deltaTime;
+
+        yaw += input.x;
+        pitch -= input.y;
+        pitch = Mathf.Clamp(pitch, -minPitch, maxPitch);
+
+        transform.rotation = Quaternion.Euler(pitch, yaw, 0f);
+    }
+}
