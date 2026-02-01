95a9eb3dd885b76fbedc45333c954ef5a39fe1de
zc5bxwr2rq-cmyk <jonreing@gmail.com>
2026-02-01 11:16:54 -0500

    Revert "somehow more changes to commit"
    This reverts commit 1a51c02c544efc6e9150e5b4c72b9e937bbb25c5, reversing
    changes made to f7f21ba90be8e78bf8a0322f607ac558272fd04e.

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -9,7 +9,7 @@ public class Interactable : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Cursor.visible = false;
+        
     }
 
     // Update is called once per frame
@@ -26,6 +26,7 @@ public class Interactable : MonoBehaviour
             interact.SetActive(true);
             player = other.gameObject;
             Enter = true;
+            
 
             if (Input.GetKeyDown(KeyCode.Space))
             {
@@ -40,7 +41,6 @@ public class Interactable : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         interact.SetActive(false);
-
         Enter = false;
     }
 
@@ -52,16 +52,12 @@ public class Interactable : MonoBehaviour
             this.gameObject.GetComponent<Template_UIManager>().Interact(this.GetComponent<VIDE_Assign>());
             this.gameObject.GetComponent<Template_UIManager>().enabled = true;
             interact.SetActive(false);
-            
-            Cursor.visible = true;
         }
     }
 
     public void ResetPlayer()
     {
         player.GetComponent<CharacterController>().enabled = true;
-
-        Cursor.visible = false;
     }
 
 
