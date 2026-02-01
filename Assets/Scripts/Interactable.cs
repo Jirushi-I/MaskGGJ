8da03f31d512d46c68ecc6f3272fa76dbefb7d31
sayu78 <yairdegeri@gmail.com>
2026-01-31 19:54:16 -0500

    interact fix

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -5,6 +5,7 @@ public class Interactable : MonoBehaviour
 {
     private GameObject player;
     public GameObject interact;
+    bool Enter;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,21 +18,40 @@ public class Interactable : MonoBehaviour
         
     }
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             //other.GetComponent<CharacterController>().enabled = false;
             interact.SetActive(true);
             player = other.gameObject;
+            Enter = true;
+            
 
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                other.GetComponent<CharacterController>().enabled = false;
+               /* other.GetComponent<CharacterController>().enabled = false;
                 this.gameObject.GetComponent<Template_UIManager>().Interact(this.GetComponent<VIDE_Assign>());
                 this.gameObject.GetComponent<Template_UIManager>().enabled = true;
-                interact.SetActive(false);
+                interact.SetActive(false);*/
             }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        interact.SetActive(false);
+        Enter = false;
+    }
+
+    public void Interact()
+    {
+        if (Enter == true)
+        {
+            player.GetComponent<CharacterController>().enabled = false;
+            this.gameObject.GetComponent<Template_UIManager>().Interact(this.GetComponent<VIDE_Assign>());
+            this.gameObject.GetComponent<Template_UIManager>().enabled = true;
+            interact.SetActive(false);
         }
     }
 
